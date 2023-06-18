db66ce65af9233390673dbe4e44da7c3465d6459
paulocunha31 <pauloroberto_odc@hotmail.com>
2023-06-18 08:58:15 -0300

    finished my segund project node

---
+++ index.js
@@ -0,0 +1,123 @@
+const express = require('express')
+const uuid = require('uuid')
+const port = 3000
+const app = express()
+app.use(express.json())
+
+const requests = []
+
+const checkRequestsId = (request, response, next) => {
+    const { id } = request.params
+
+    const index = requests.findIndex(user => user.id === id)
+    
+    if(index < 0){
+        return response.status(404).json({erro: "user not found"})
+    }
+
+    request.orderIndex = index
+    request.orderId = id
+    
+
+    next()
+}
+
+const methodAndUrl = (request, response, next) =>{
+    console.log(" Methodo: " + request.method)
+    console.log(" Url: " + request.url)
+
+    next()
+}
+
+
+app.get('/users', methodAndUrl, (request, response) => {
+    return response.json({requests})
+    
+
+})
+
+app.get('/users/:id',methodAndUrl, (request, response) => {
+    const {id} = request.params
+   
+    const index = requests.findIndex(user => user.id ===id)
+
+    if(index < 0 ){
+        return response.status(404).json({erro: "user not found"})
+    }
+
+    const orderInProcess = requests.find(user => user.id === id)
+
+   
+    return response.json(orderInProcess)
+})
+
+app.post('/users', methodAndUrl, (request, response) => {
+   
+    const { order, clienteName, price, status } = request.body
+
+    const user = {id: uuid.v4(), order, clienteName, price, status}
+
+    
+    
+    requests.push(user)
+    return response.status(201).json(user)
+})
+
+app.put('/users/:id', checkRequestsId, methodAndUrl, (request, response) => {
+    const {order, clienteName, price, status} = request.body
+    const index = request.orderIndex
+    const id = request.orderId
+
+    const updateUser = { id, order, clienteName, price, status}
+
+    requests[index] = updateUser
+   
+    return response.json(updateUser)
+})
+
+app.delete('/users/:id', checkRequestsId, methodAndUrl, (request, response) => {
+    const index = request.orderIndex
+   
+    requests.splice(index,1)
+
+    return response.status(202).json({Ok: "successfully deleted"})
+})
+
+
+app.patch('/users/:id', checkRequestsId, methodAndUrl, (request, response) => {
+    const  index = request.orderIndex
+    const  id = request.orderId
+
+    const newRequests = requests.filter( item => item.id ===id)
+
+   newRequests.forEach( updateStatus=> {
+      updateStatus.status = "pronto"
+
+     requests[index] = updateStatus
+    
+    return response.json( updateStatus)
+    
+   });
+  
+
+ 
+ 
+
+})
+
+   
+
+
+
+
+
+
+
+
+
+app.listen(port, () => {
+    console.log(`👍 Servidor status on port ${port}`)
+})
+
+
+
